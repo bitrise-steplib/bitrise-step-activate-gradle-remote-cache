5c9f81a92d397784508e3063d554a528a1a0f641
Olivér Falvai <oliver.falvai@bitrise.io>
2022-11-04 12:04:24 +0100

    Generate init script

---
+++ step/step_test.go
@@ -0,0 +1,96 @@
+package step
+
+import (
+	"fmt"
+	"io/ioutil"
+	"path"
+	"testing"
+
+	"github.com/bitrise-io/go-steputils/v2/stepconf"
+	"github.com/bitrise-io/go-utils/v2/command"
+	"github.com/bitrise-io/go-utils/v2/env"
+	"github.com/bitrise-io/go-utils/v2/log"
+)
+
+func TestRemoteCacheStep_Run(t *testing.T) {
+	tests := []struct {
+		name    string
+		envRepo env.Repository
+		wantErr bool
+	}{
+		{
+			name: "happy path",
+			envRepo: fakeEnvRepo{envVars: map[string]string{
+				"BITRISEIO_BITRISE_SERVICES_ACCESS_TOKEN": "fake access token",
+				"verbose": "false",
+			}},
+		},
+		{
+			name: "missing auth token",
+			envRepo: fakeEnvRepo{envVars: map[string]string{
+				"verbose": "false",
+			}},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			fakePath := path.Join(t.TempDir(), "init.gradle")
+			step := RemoteCacheStep{
+				logger:         log.NewLogger(),
+				inputParser:    stepconf.NewInputParser(tt.envRepo),
+				commandFactory: command.NewFactory(tt.envRepo),
+				envRepo:        tt.envRepo,
+				pathModifier:   fakePathModifier{path: fakePath},
+			}
+			err := step.Run()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Run() error = %v, wantErr %v", err, tt.wantErr)
+			} else if err == nil {
+				_, err := ioutil.ReadFile(fakePath)
+				if err != nil {
+					t.Errorf("failed to open generated file: %s", err)
+				}
+			}
+		})
+	}
+}
+
+type fakeEnvRepo struct {
+	envVars map[string]string
+}
+
+func (repo fakeEnvRepo) Get(key string) string {
+	value, ok := repo.envVars[key]
+	if ok {
+		return value
+	} else {
+		return ""
+	}
+}
+
+func (repo fakeEnvRepo) Set(key, value string) error {
+	repo.envVars[key] = value
+	return nil
+}
+
+func (repo fakeEnvRepo) Unset(key string) error {
+	repo.envVars[key] = ""
+	return nil
+}
+
+func (repo fakeEnvRepo) List() []string {
+	envs := []string{}
+	for k, v := range repo.envVars {
+		envs = append(envs, fmt.Sprintf("%s=%s", k, v))
+	}
+	return envs
+}
+
+type fakePathModifier struct {
+	path string
+}
+
+func (pm fakePathModifier) AbsPath(pth string) (string, error) {
+	return pm.path, nil
+}
