f51a7a13595b63c68f3267e3245c1b4e011deac7
Viktor Benei <viktorbenei@bitrise.io>
2024-01-31 16:40:34 +0100

    Check if the workspace has an active Bitrise Build Cache trial or subscription (#24)
    Print error and add annotation if the workspace doesn't have an active build cache
    trial or subscription, with
    instructions how to activate a trial or contact us.

--- step.sh
+++ step.sh
@@ -1,5 +1,33 @@
 #!/usr/bin/env bash
-set -euxo pipefail
+
+# 'read' has to be before 'set -e'
+read -r -d '' UNAVAILABLE_MESSAGE << EOF_MSG
+Bitrise Build Cache is not activated in this build.
+
+You have added the **Activate Bitrise Build Cache for Gradle** add-on step to your workflow.
+
+However, you don't have an activate Bitrise Build Cache Trial or Subscription for the current workspace yet.
+
+You can activate a Trial at [app.bitrise.io/build-cache](https://app.bitrise.io/build-cache),
+or contact us at [support@bitrise.io](mailto:support@bitrise.io) to activate it.
+EOF_MSG
+
+set -eo pipefail
+
+echo "Checking whether Bitrise Build Cache is activated for this workspace ..."
+if [ "$BITRISEIO_BUILD_CACHE_ENABLED" != "true" ]; then
+  printf "\n%s\n" "$UNAVAILABLE_MESSAGE"
+  set -x
+  bitrise plugin install https://github.com/bitrise-io/bitrise-plugins-annotations.git
+  bitrise :annotations annotate "$UNAVAILABLE_MESSAGE" --style error || {
+    echo "Failed to create annotation"
+    exit 3
+  }
+  exit 2
+fi
+echo "Bitrise Build Cache is activated in this workspace, configuring the build environment ..."
+
+set -x
 
 # download the Bitrise Build Cache CLI
 export BITRISE_BUILD_CACHE_CLI_VERSION="v0.8.0"
