61852ef0673ebb2687dbc9bf0d3c6de86b915ce8
Viktor Benei <viktorbenei@bitrise.io>
2024-01-31 11:40:55 +0100

    Added `--retry 5` to `curl`

--- step.sh
+++ step.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 # download the Bitrise Build Cache CLI
 export BITRISE_BUILD_CACHE_CLI_VERSION="v0.8.0"
-curl -sSfL 'https://raw.githubusercontent.com/bitrise-io/bitrise-build-cache-cli/main/install/installer.sh' | sh -s -- -b /tmp/bin -d $BITRISE_BUILD_CACHE_CLI_VERSION
+curl --retry 5 -sSfL 'https://raw.githubusercontent.com/bitrise-io/bitrise-build-cache-cli/main/install/installer.sh' | sh -s -- -b /tmp/bin -d $BITRISE_BUILD_CACHE_CLI_VERSION
 
 # run the Bitrise Build Cache CLI
 /tmp/bin/bitrise-build-cache enable-for gradle
