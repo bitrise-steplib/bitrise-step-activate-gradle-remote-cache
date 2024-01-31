4486284871da29b4d774b0052a7057258b970472
Viktor Benei <viktorbenei@bitrise.io>
2024-01-31 10:28:17 +0100

    Specify the CLI version to download.

--- step.sh
+++ step.sh
@@ -2,7 +2,8 @@
 set -euxo pipefail
 
 # download the Bitrise Build Cache CLI
-curl -sSfL 'https://raw.githubusercontent.com/bitrise-io/bitrise-build-cache-cli/main/install/installer.sh' | sh -s -- -b /tmp/bin -d
+export BITRISE_BUILD_CACHE_CLI_VERSION="v0.8.0"
+curl -sSfL 'https://raw.githubusercontent.com/bitrise-io/bitrise-build-cache-cli/main/install/installer.sh' | sh -s -- -b /tmp/bin -d $BITRISE_BUILD_CACHE_CLI_VERSION
 
 # run the Bitrise Build Cache CLI
 /tmp/bin/bitrise-build-cache enable-for gradle
