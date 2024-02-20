6e8e6cf0386ee48a18b505eea177cc2e60d67809
zsolt-marta-bitrise <zsolt.marta@bitrise.io>
2024-02-20 13:16:42 +0100

    chore: Bump CLI version to 0.8.3

--- step.sh
+++ step.sh
@@ -30,7 +30,7 @@ echo "Bitrise Build Cache is activated in this workspace, configuring the build 
 set -x
 
 # download the Bitrise Build Cache CLI
-export BITRISE_BUILD_CACHE_CLI_VERSION="v0.8.2"
+export BITRISE_BUILD_CACHE_CLI_VERSION="v0.8.3"
 curl --retry 5 -sSfL 'https://raw.githubusercontent.com/bitrise-io/bitrise-build-cache-cli/main/install/installer.sh' | sh -s -- -b /tmp/bin -d $BITRISE_BUILD_CACHE_CLI_VERSION
 
 # run the Bitrise Build Cache CLI
