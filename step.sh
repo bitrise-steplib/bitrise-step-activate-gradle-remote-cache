abc29808148706835e5152226d87e3fd492351da
LaszloP <7979773+lpusok@users.noreply.github.com>
2024-06-25 10:30:39 +0200

    Use correct debug flag

--- step.sh
+++ step.sh
@@ -50,5 +50,5 @@ if [ "$verbose" != "true" ] && [ "$verbose" != "false" ]; then
 fi
 
 # run the Bitrise Build Cache CLI
-/tmp/bin/bitrise-build-cache enable-for gradle --metrics="$collect_metrics" --push="$push" --validation-level="$validation_level" --verbose="$verbose"
+/tmp/bin/bitrise-build-cache enable-for gradle --metrics="$collect_metrics" --push="$push" --validation-level="$validation_level" --debug="$verbose"
 
